713e076e8bbdca137927d71d29babeec3c5d316f
Pataar <pietering1@gmail.com>
2016-06-12 17:15:27 +0200

    Added initial code

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # logar
-Simple node logger
+Logar is a simple node.js stdout logger. 
+```sh
+npm install logar --save
+```
+
+## Usage
+Logar can be used like this:
+
+
+Just require the package:
+```javascript
+var logar = require("logar")("MyApp"[, enableTimestamps]);
+```
+"MyApp" will be used as keyword. You can leave this empty if you want. If you want to disable timestamps, pass 'false' as second argument.
+
+#### Basic functions
+These are the basic functions of Logar. They have colored output in the console.
+
+```javascript
+logar.i("this is an info message"); //ouput an info message
+logar.e("this is an error "); //log an error
+logar.success("this is a success message"); //log a success message
+logar.ll("this is a light message"); //log a light message
+logar.warn("this is a warning"); //log a warning
+logar.nl(); //create a new line
+```
+Example output:
+```sh
+[16:44:37] MyApp: ✓ this is a success message
+```
+
+
+#### Loading function
+Logar also has a loading function. It can be used like this:
+```javascript
+//create a loader instance
+var loader = logar.loading("Creating user schemes");
+
+//async call
+createSchemesAsync().then(function () {
+    //pass the success message if it succeeds (optional)
+	loader("Created user schemes succesfully");
+}, function (err) {
+    //pass an error message/object as second parameter if something went wrong
+    //or use 'true' as second parameter
+	loader("Error while creating user schemes", err);
+});
+```
---
+++ main.js
@@ -0,0 +1,119 @@
+const chalk      = require('chalk'),
+      dateformat = require('dateformat'),
+      _          = require("lodash"),
+      readline   = require('readline');
+
+module.exports = (keyword, enableTimestamp = true) => {
+
+	if (typeof process.env.LOGAR_KEYWORD !== 'undefined') {
+		keyword = process.env.LOGAR_KEYWORD;
+	}
+
+	function getTimestamp() {
+		return '[' + chalk.grey(dateformat(new Date(), 'HH:MM:ss')) + ']';
+	}
+
+	function baseLog(isError) {
+		if (enableTimestamp) {
+			var time = getTimestamp();
+			process.stdout.write(time + ' ');
+		}
+
+		if (isError) {
+			process.stdout.write(chalk.bold.red(keyword + ' error: '));
+		} else {
+			process.stdout.write(chalk.bold.cyan(keyword + ': '));
+		}
+	}
+
+	var logar = {
+		//info
+		i: (...args) => {
+			baseLog();
+			args[0] = chalk.yellow(args[0]);
+			if (args.length > 1) {
+				for (arg in args) {
+					args[arg] = calk.green(arg);
+				}
+			}
+
+			console.log.apply(console, args);
+			return this;
+		},
+
+		//error
+		e: (e) => {
+			baseLog(true);
+			console.error(e);
+			return this;
+		},
+
+		success: (...args) => {
+			baseLog();
+			args[0] = chalk.bold.green("✓ " + args[0]);
+			if (args.length > 1) {
+				args[1] = chalk.yellow(args[1]);
+			}
+			console.log.apply(console, args);
+			return this;
+		},
+
+		warn: (...args) => {
+			baseLog();
+			args[0] = chalk.bgYellow.red(" ❗ " + args[0] + " ");
+			if (args.length > 1) {
+				args[1] = chalk.yellow(args[1]);
+			}
+			console.log.apply(console, args);
+			return this;
+		},
+
+		nl: () => {
+			process.stdout.write("\n");
+		},
+
+		ll: (...args) => {
+			baseLog();
+			args[0] = chalk.bold.grey(args[0]);
+
+			if (args.length > 1) {
+				args[1] = chalk.grey(args[1]);
+			}
+			console.log.apply(console, args);
+			return this;
+		},
+
+		loading: (message) => {
+
+			var i               = 0,
+			    loadingInterval = setInterval(function () {
+				    process.stdout.cursorTo(0);
+				    process.stdout.clearLine();
+				    baseLog();
+				    i = (i + 1) % 4;
+				    var dots = new Array(i + 1).join(".");
+				    process.stdout.write(chalk.green(message + dots)); // write text
+			    }, 200);
+
+			return function (completeMessage, err) {
+				// console.log(rl);
+				clearInterval(loadingInterval);
+				process.stdout.cursorTo(0); // move cursor to beginning of line
+				process.stdout.clearLine(); // clear current text
+
+				if (completeMessage && !err) {
+					logar.success(completeMessage);
+				} else if (completeMessage && err) {
+					logar.e(completeMessage);
+					if (err !== true) {
+						logar.e(err);
+					}
+				}
+
+				return true;
+			};
+		}
+	};
+
+	return logar;
+};
